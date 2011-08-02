c8915494369a1593496efe1b60220ca9b4277483
cameel <cameel2@gmail.com>
2011-08-02 20:56:40 +0200

    Make the plugin an Engine

--- lib/yacaph.rb
+++ lib/yacaph.rb
@@ -1,4 +1,7 @@
-$:.unshift "#{File.dirname(__FILE__)}/lib"
+# FIXME: Use an assertion
+fail if !defined?(Rails)
+
+require 'yacaph/engine' if Rails::VERSION::MAJOR >= 3
 
 require 'captcha_util'
 require 'captcha_helper'
---
+++ lib/yacaph/engine.rb
@@ -0,0 +1,7 @@
+require 'rails'
+
+module Yacaph
+  class Engine < Rails::Engine
+    engine_name :yacaph
+  end
+end
