bbf3906721b28de8eb16fa21636642c6c4c2ad70
mondoreale <mariusz@mondoreale.pl>
2011-09-09 18:09:30 +0200

    captcha_validated? parametrized

--- lib/yacaph/validates_captcha.rb
+++ lib/yacaph/validates_captcha.rb
@@ -11,8 +11,14 @@ module ValidatesCaptcha
   end
 
   module InstanceMethods
-    def captcha_validated?
-       CaptchaUtil::encrypt_string(params[:captcha].to_s.gsub(' ', '').downcase) == params[:captcha_validation]
+    def captcha_validated?(options = {})
+       captcha = options.delete :captcha
+       captcha_validaton = options.delete :captcha_validation
+       {:captcha => captcha, :captcha_validation => captcha_validation}.each do |k, v|
+         raise ArgumentError.new "#{k} has to be a String" unless v.is_a?(String)
+       end
+       raise ArgumentError.new 'To many argument specified' unless options.empty?
+       CaptchaUtil::encrypt_string(captcha.to_s.gsub(' ', '').downcase) == captcha_validation
     end
   end
 end
