de2c2e3252baf2ee38cde4b09f6cce531d43d8f5
mondoreale <mariusz@mondoreale.pl>
2011-09-09 19:14:20 +0200

    Fixed typo

--- lib/yacaph/validates_captcha.rb
+++ lib/yacaph/validates_captcha.rb
@@ -13,11 +13,11 @@ module ValidatesCaptcha
   module InstanceMethods
     def captcha_validated?(options = {})
        captcha = options.delete :captcha
-       captcha_validaton = options.delete :captcha_validation
+       captcha_validation = options.delete :captcha_validation
        {:captcha => captcha, :captcha_validation => captcha_validation}.each do |k, v|
          raise ArgumentError.new "#{k} has to be a String" unless v.is_a?(String)
        end
-       raise ArgumentError.new 'To many argument specified' unless options.empty?
+       raise ArgumentError.new 'To many arguments specified' unless options.empty?
        CaptchaUtil::encrypt_string(captcha.to_s.gsub(' ', '').downcase) == captcha_validation
     end
   end
--- yacaph.gemspec
+++ yacaph.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name             = 'yacaph'
-  s.version          = "0.2.2"
+  s.version          = "0.2.1"
   s.date             = "2011-09-07"
   s.platform         = Gem::Platform::RUBY
 
