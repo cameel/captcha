dfe30f58acdbf4681e75936fe18885f4f51a3b89
mondoreale <mariusz@mondoreale.pl>
2011-09-09 19:55:18 +0200

    Changed the way that captcha_validated? arguments are verified

--- lib/yacaph/validates_captcha.rb
+++ lib/yacaph/validates_captcha.rb
@@ -14,10 +14,8 @@ module ValidatesCaptcha
     def captcha_validated?(options = {})
        captcha = options.delete :captcha
        captcha_validation = options.delete :captcha_validation
-       {:captcha => captcha, :captcha_validation => captcha_validation}.each do |k, v|
-         raise ArgumentError.new "#{k} has to be a String" unless v.is_a?(String)
-       end
-       raise ArgumentError.new 'To many arguments specified' unless options.empty?
+       raise ArgumentError.new ':captcha has to be a String' unless captcha.is_a?(String)
+       raise ArgumentError.new ':captcha_validation has to be a String' unless captcha_validation.is_a?(String)
        CaptchaUtil::encrypt_string(captcha.to_s.gsub(' ', '').downcase) == captcha_validation
     end
   end
